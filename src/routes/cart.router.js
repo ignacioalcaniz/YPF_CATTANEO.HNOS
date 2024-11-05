e3e94cb840e4059d66fa110ef6be3f3bf51fc10b
ignacio alcaniz <ignaalcaniz@gmail.com>
2024-11-05 18:47:33 -0300

    tercer commit

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -30,35 +30,6 @@ cartRouter.post("/:idCart/product/:idProd", async (req, res) => {
     try {
         const { idProd } = req.params;
         const { idCart } = req.params;
-
-        const cartExists = await CartManager.getCartById(idCart);
-        if (!cartExists) {
-            return res.status(404).json({ message: 'Cart not found' });
-        }
-
-        const prodExists = await prodManager.getProductbyId(idProd);
-        if (!prodExists) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
-
-        const existsProdInCart = cartExists.products.find(prod => prod.id === idProd);
-
-        if (!existsProdInCart) {
-
-            const product = {
-                id: idProd,
-                nombre:prodExists.nombre,
-                precio:prodExists.precio,
-                personas:prodExists.cantidad_personas,
-                imagen:prodExists.thumbnails,
-                quantity:1
-                
-            };
-            cartExists.products.push(product);
-        } else {
-
-            existsProdInCart.quantity += 1;
-        }
         const response = await CartManager.saveProductToCart(idCart, idProd);
         res.json(response);
     } catch (error) {
