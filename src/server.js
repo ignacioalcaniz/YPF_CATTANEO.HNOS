2de397f07679326b89a684eff0d8acdec63b9f16
ignacio alcaniz <ignaalcaniz@gmail.com>
2024-11-11 15:30:00 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { urlencoded } from "express";
-import {packageRouter } from "./routes/products.router.js";
+import { packageRouter } from "./routes/products.router.js";
 import { UserRouter } from "./routes/user.router.js";
 import { cartRouter } from "./routes/cart.router.js";
 import path from 'path';
@@ -11,39 +11,39 @@ import { prodManager } from "./Manager/products.manager.js";
 
 
 const app = express();
-app.engine("handlebars",handlebars.engine());
-app.set("views",path.join(process.cwd(),"views"))
-app.set("view engine","handlebars")
-app.use("/", express.static(path.join(process.cwd(),"public")));
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(process.cwd(), "views"))
+app.set("view engine", "handlebars")
+app.use("/", express.static(path.join(process.cwd(), "public")));
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use("/api/products",packageRouter);
-app.use("/api/users",UserRouter);
-app.use("/api/cart",cartRouter);
-app.use("/",viewRoutes);
+app.use("/api/products", packageRouter);
+app.use("/api/users", UserRouter);
+app.use("/api/cart", cartRouter);
+app.use("/", viewRoutes);
 
- const httpServer=app.listen(3000, () => console.log("listening on port 3000"))
- const socketServer=new Server(httpServer)
+const httpServer = app.listen(3000, () => console.log("listening on port 3000"))
+const socketServer = new Server(httpServer)
 
- socketServer.on("connection",async(socket)=>{
+socketServer.on("connection", async (socket) => {
     console.log("new connection :", socket.id)
 
-    socket.on("disconnect",()=>{
-        console.log("user disconnected:",socket.id)
+    socket.on("disconnect", () => {
+        console.log("user disconnected:", socket.id)
 
     })
-    socketServer.emit("allProducts",await prodManager.getAll())
-    
+    socketServer.emit("allProducts", await prodManager.getAll())
 
-    socket.on("newProduct",async(product)=>{
+
+    socket.on("newProduct", async (product) => {
         await prodManager.createProduct(product)
-        socketServer.emit("productos",await prodManager.getAll())
-        })
-        socket.on("deleteProduct",async(productId)=>{
-            await prodManager.deleteById(productId)
-            socketServer.emit("updateProducts",await prodManager.getAll())
-        })
+        socketServer.emit("productos", await prodManager.getAll())
+    })
+    socket.on("deleteProduct", async (productId) => {
+        await prodManager.deleteById(productId)
+        socketServer.emit("updateProducts", await prodManager.getAll())
+    })
 })
 
 
-   
+
